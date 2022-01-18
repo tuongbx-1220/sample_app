34cf784bc7f1a0aa43b39217345ce370a703b285
buixuantuong <bui.xuan.tuong@sun-asterisk.com>
2022-01-18 15:08:40 +0700

    Chapter 12 -- Reset password

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,61 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, :valid_user, :check_expiration, only: %i(edit update)
+
+  def new; end
+
+  def create
+    @user = User.find_by email: params[:password_reset][:email].downcase
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t "email_sent_reset_password"
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "email_not_found"
+      render :new
+    end
+  end
+
+  def update
+    if params[:user][:password].blank?
+      @user.errors.add :password, t("cannot_blank")
+      render :edit
+    elsif @user.update user_params
+      log_in @user
+      flash[:success] = t "password_reseted"
+      redirect_to @user
+    else
+      flash[:danger] = t "reset_failed"
+      render :edit
+    end
+  end
+
+  def edit; end
+
+  private
+
+  def get_user
+    @user = User.find_by email: params[:email]
+    return if @user
+
+    flash[:danger] = t "error.user.not_found"
+    redirect_to root_url
+  end
+
+  def valid_user
+    return if @user.activated && @user.authenticated?(:reset, params[:id])
+
+    redirect_to root_url
+  end
+
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = t "reset_expired"
+    redirect_to new_password_reset_url
+  end
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
 
     resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index destroy)
   end
 end
